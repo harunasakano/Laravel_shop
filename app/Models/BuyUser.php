c1729c5a1bea845ea1e77a8d38349d2c95ca8e82
Sakanoharuna <kismyftshft@gmail.com>
2018-11-11 22:36:04 +0900

    Model name 修正

--- app/Models/BuyUser.php
+++ app/Models/BuyUser.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Buy_users extends Model
+class BuyUser extends Model
 {
     //
-}
+}     
--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Purchases extends Model
+class Purchase extends Model
 {
     protected $filable = ['id','Userid','itemCode','type','buyDate','created_at','updated_at'];
 }
