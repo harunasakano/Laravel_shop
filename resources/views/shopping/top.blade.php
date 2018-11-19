8415e40420061fafe4689700528596750c124368
Sakanoharuna <kismyftshft@gmail.com>
2018-11-20 06:11:45 +0900

    バリデーション及びデザイン修正

--- resources/views/shopping/top.blade.php
+++ resources/views/shopping/top.blade.php
@@ -10,27 +10,29 @@
         <div class="container">
             <h1>LaraShop</h1>
             <form name="queryform" action="{{ url('/search') }}" method="GET" id="queryform">
-            <div class="searchForm">
-                キーワード検索：
+            <p class="formTitle">カテゴリ</p>
+            <div class="categoryBox cp_sl01">
                 <select name="category_id" id="category_id" required/>
                     @foreach ($categories as $category_id => $category)
                         <option value="{{{ $category_id }}}">{{{ $category }}}</option>
                     @endforeach
                 </select>
             </div>
-            <div class="sortForm">
-              表示順序:
+            <p class="formTitle">表示順</p>
+            <div class="sortBox cp_sl01">
               <select name="sort" id="sort" required/>
                   @foreach ($sortOrders as $sortOrder_key => $sortOrder)
                       <option value="{{{ $sortOrder_key }}}">{{{ $sortOrder }}}</option>
                   @endforeach
               </select>
             </div>
-            <div class="sortForm">
-            <input class="search-box" type="text" name="query_word" value="" id="query_word" placeholder="キーワードを入力">
+            <p class="formTitle">検索キーワード&ensp;<span class="requiredtext">※必須</span></p>
+            <div class="wordsBox">
+              <label class="textef">
+                <input type="text" name="query_word" value="" id="query_word" placeholder="キーワードを入力">
             </div>
             <div class="searchButton">
-                <button class="search-button" type="submit" id="button">Yahooショッピングで検索</button>
+                <button class="search-button" type="submit" id="button">ショップで検索</button>
             </div>
             </form>
         </div>
