60585981ebc523455cb690c3cd7c692470105490
Sakanoharuna <kismyftshft@gmail.com>
2018-11-22 11:14:40 +0900

    エラー画面修正

--- resources/views/shopping/error.blade.php
+++ resources/views/shopping/error.blade.php
@@ -7,8 +7,12 @@
     <body>
         <div class="container">
             <h1><span class="maintitle">LaraShop</span></h1>
-            <div class="errortext">ごめんなさい。検索キーワードに一致する商品がありません。<br>
+            <div class="errortext">ごめんなさい。検索キーワードに一致する商品がありませんでした。<br>
               違うワードで検索してみてください。
             </div>
+            <div class="errorBacktop">
+               <a href={{ url('/') }}>TOPに戻る</a>
+            </div>
+        </div>
     </body>
 </html>
